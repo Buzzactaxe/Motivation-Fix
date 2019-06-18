da3d4410b7479ba593ce010ef6d4a85eedefc478
Buzzactaxe <valiantsimpson@gmail.com>
2019-06-18 09:33:41 +0200

    git delete old heroku URL comment

--- app.js
+++ app.js
@@ -35,22 +35,6 @@ mongoose
 		console.log('ERROR:', err.message);
 	});
 
-//Heroku Environment DB
-// mongoose
-// 	.connect(
-// 		'mongodb+srv://buzzactaxe:buzzactaxe88@cluster0-co6r4.mongodb.net/motivationfix?retryWrites=true&w=majority',
-// 		{
-// 			useNewUrlParser: true,
-// 			useCreateIndex: true
-// 		}
-// 	)
-// 	.then(() => {
-// 		console.log('Connected to DB!');
-// 	})
-// 	.catch((err) => {
-// 		console.log('ERROR:', err.message);
-// 	});
-
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(
